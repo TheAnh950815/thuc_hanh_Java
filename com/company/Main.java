f7525ed34d9520c7c205ce9cc0727a08f5d19dba
TheAnh950815 <theanh950815@gmail.com>
2021-02-04 14:15:13 +0700

    thuc_hanh

---
+++ com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Date;
+
+class SystemTime {
+
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
